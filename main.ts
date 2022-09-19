b70c66b5ddcedda898706708917e9a0d98094751
timflee <timflee@hotmail.com>
2022-09-19 23:57:53 +1000

    Added some silly sounds nad buton presses

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-basic.forever(function on_forever() {
-    basic.showString("a ")
-    basic.showString("b ")
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
+    count += -1
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(523, music.beat(BeatFraction.Sixteenth))
+    count += 1
+})
+let count = 0
+music.playMelody("C5 G B A F A C5 B ", 1000)
+basic.forever(function () {
+    basic.showNumber(count)
+    if (count % 3 == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
 })
